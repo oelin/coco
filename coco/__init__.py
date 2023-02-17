ad566ec794e1fbb95595d774c6506cb1b8f6cfca
kale <42823429+oelin@users.noreply.github.com>
2023-02-17 12:34:34 +0000

    Create __init__.py

---
+++ coco/__init__.py
@@ -0,0 +1,24 @@
+from typing import Callable, Any
+import dis
+import marshal
+import pickle
+
+
+Data = Any
+Decoder = Callable[None, Callable]
+
+
+def encode(data: Data, decoder: Decoder) -> Data:
+
+        function = dis.Bytecode(function)
+        function = marshal.dumps(decoder.codeobj)
+
+        return pickle.dumps((data, decoder))
+
+
+def decode(data: Data) -> Data:
+
+        data, decoder = pickle.loads(data)
+        decoder = marshal.loads(decoder)
+
+        return eval(decoder)(data)
